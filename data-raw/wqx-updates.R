127418089a57fa14f5ec7e35faa8a4c13726dc85
ergz <erodriguez@flowwest.com>
2020-08-27 10:10:03 -0700

    adds more pre-processing for wqx uploads

--- data-raw/wqx-updates.R
+++ data-raw/wqx-updates.R
@@ -35,17 +35,77 @@ baro_air %>%
 
 
 # EPO1 ------------------------------------------------------------
+# 440965 TS
 
-# 440965
+# stream stage
 epo_stream <- wqx_csv_results(con,
                             location = "EPO1",
                             characteristic = "Stream Stage",
                             file_path = "../DATA/physical-results/epo1-stream-stage_results.csv")
 
+epo_stream %>%
+  select(`Activity Start Date`, `Activity Start Time`, `Result Value`)
+
+epo_stream %>%
+  group_by(
+    location_id = `Monitoring Location ID`,
+    chars_name = `Characteristic Name`) %>%
+  summarise(
+    total = n(),
+    start_date = min(as_date(`Activity Start Date`)),
+    end_date = max(as_date(`Activity Start Date`))
+  ) %>% ungroup()
 
 epo_stream %>%
   ggplot(aes(as_date(`Activity Start Date`), `Result Value`)) +
-  geom_point(alpha=0.1)
+  geom_point(alpha = 0.1)
+
+# air temperature
+epo_air <- wqx_csv_results(con,
+                              location = "EPO1",
+                              characteristic = "Temperature, air",
+                              file_path = "../DATA/physical-results/epo1-air-temp_results.csv")
+
+epo_air %>%
+  select(`Activity Start Date`, `Activity Start Time`, `Result Value`)
+
+epo_air %>%
+  group_by(
+    location_id = `Monitoring Location ID`,
+    chars_name = `Characteristic Name`) %>%
+  summarise(
+    total = n(),
+    start_date = min(as_date(`Activity Start Date`)),
+    end_date = max(as_date(`Activity Start Date`))
+  ) %>% ungroup()
+
+epo_air %>%
+  ggplot(aes(as_date(`Activity Start Date`), `Result Value`)) +
+  geom_point(alpha = 0.1)
+
+
+# water temperature
+epo_water <- wqx_csv_results(con,
+                           location = "EPO1",
+                           characteristic = "Temperature, water",
+                           file_path = "../DATA/physical-results/epo1-water-temp_results.csv")
+
+epo_water %>%
+  select(`Activity Start Date`, `Activity Start Time`, `Result Value`)
+
+epo_water %>%
+  group_by(
+    location_id = `Monitoring Location ID`,
+    chars_name = `Characteristic Name`) %>%
+  summarise(
+    total = n(),
+    start_date = min(as_date(`Activity Start Date`)),
+    end_date = max(as_date(`Activity Start Date`))
+  ) %>% ungroup()
+
+epo_water %>%
+  ggplot(aes(as_date(`Activity Start Date`), `Result Value`)) +
+  geom_point(alpha = 0.1)
 
 
 # VY1 -----------------------------------------------------------
